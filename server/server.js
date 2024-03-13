4a22519ebe5c4d0fc80acaa69f93e8700382172d
Vishal21121 <vs84077000@gmail.com>
2024-03-13 21:19:54 +0530

    added: route for getting random movie

--- server/server.js
+++ server/server.js
@@ -17,42 +17,42 @@ app.use(cors())
 // it handles form data
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/:id", (req, res) => {
-    const { id } = req.params
-    // Ensure there is a range given for the video
-    const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range header");
-    }
+// app.get("/:id", (req, res) => {
+//     const { id } = req.params
+//     // Ensure there is a range given for the video
+//     const range = req.headers.range;
+//     if (!range) {
+//         res.status(400).send("Requires Range header");
+//     }
 
-    // get video stats (about 61MB)
-    const videoPath = `video-${id}.mp4`;
-    const videoSize = fs.statSync(videoPath).size;
+//     // get video stats (about 61MB)
+//     const videoPath = `video-${id}.mp4`;
+//     const videoSize = fs.statSync(videoPath).size;
 
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//     // Parse Range
+//     // Example: "bytes=32324-"
+//     const CHUNK_SIZE = 10 ** 6; // 1MB
+//     const start = Number(range.replace(/\D/g, ""));
+//     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-    // Create headers
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
+//     // Create headers
+//     const contentLength = end - start + 1;
+//     const headers = {
+//         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//         "Accept-Ranges": "bytes",
+//         "Content-Length": contentLength,
+//         "Content-Type": "video/mp4",
+//     };
 
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers);
+//     // HTTP Status 206 for Partial Content
+//     res.writeHead(206, headers);
 
-    // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(videoPath, { start, end });
+//     // create video read stream for this particular chunk
+//     const videoStream = fs.createReadStream(videoPath, { start, end });
 
-    // Stream the video chunk to the client
-    videoStream.pipe(res);
-})
+//     // Stream the video chunk to the client
+//     videoStream.pipe(res);
+// })
 
 app.post("/upload", upload.single("video"), async (req, res) => {
     console.log(req.body)
@@ -89,6 +89,38 @@ app.post("/upload", upload.single("video"), async (req, res) => {
             message: error || "Internal server error"
         }
     })
+})
+
+app.get("/video", async (req, res) => {
+    // get random video from db
+    try {
+        const video = await Video.aggregate([{ $sample: { size: 1 } }])
+        if (video) {
+            return res.status(200).json({
+                success: true,
+                data: {
+                    successCode: 200,
+                    message: "video fetched successfully",
+                    video: video
+                }
+            })
+        }
+        return res.status(404).json({
+            success: false,
+            data: {
+                successCode: 404,
+                message: "video not found"
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: {
+                successCode: 500,
+                message: error || "Internal server error"
+            }
+        })
+    }
 })
 
 app.listen(PORT, () => {
