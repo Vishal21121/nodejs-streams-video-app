5b469233afe85c11b3af20fd62bfd369ccef0a11
Vishal21121 <vs84077000@gmail.com>
2024-03-15 14:55:17 +0530

    added: react-player

--- client/src/components/VideoScreen.jsx
+++ client/src/components/VideoScreen.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import ReactPlayer from "react-player"
 
 
 function VideoScreen() {
-    const vidoeRef = useRef(null)
-    const videoElement = useRef(null)
+    const [url, setUrl] = useState("")
 
     const handleClick = async () => {
         const response = await fetch(`http://localhost:8080/video`, {
@@ -12,8 +12,7 @@ function VideoScreen() {
             }
         });
         const data = await response.json()
-        vidoeRef.current.src = data.data.video.videoUrl
-        videoElement.current.load()
+        setUrl(data.data.video.videoUrl)
     }
 
     useEffect(() => {
@@ -24,9 +23,13 @@ function VideoScreen() {
         <div className="w-full flex justify-center p-8 h-[90vh]">
             <div className="w-1/2 mockup-window border bg-base-300 ">
                 <div className="flex flex-col items-center gap-8 px-4 py-8 bg-base-200">
-                    <video ref={videoElement} className="w-full" autoPlay controls>
-                        <source ref={vidoeRef} src="" />
-                    </video>
+                    <ReactPlayer
+                        url={url}
+                        playing
+                        controls
+                        light
+                        onEnded={handleClick}
+                    />
                     <button onClick={handleClick} className="w-fit btn btn-primary">Next</button>
                 </div>
             </div>
